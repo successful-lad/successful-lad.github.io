10e4d9dd7b1e58109b9ddeb16f1d6f4f1da36318
egorglebenko <blessedonwebdev@gmail.com>
2020-03-08 16:01:05 +0200

    create build-v

---
+++ precache-manifest.eaa56980d4ee5a78831deacc434d3f7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44db03a6fe5b0887514abef9ca364f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d31bcf4fad430b067d",
+    "url": "/static/css/main.495541a0.chunk.css"
+  },
+  {
+    "revision": "3efe71a589741457cf0b",
+    "url": "/static/js/2.78668ca3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.78668ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d31bcf4fad430b067d",
+    "url": "/static/js/main.6595f338.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "7eaa526affaf4669147310679f48e34b",
+    "url": "/static/media/bed.7eaa526a.svg"
+  },
+  {
+    "revision": "b66fdd79197493b225f85699fafe7d51",
+    "url": "/static/media/buy.b66fdd79.svg"
+  },
+  {
+    "revision": "8b0126c677112ff8ac1c581569efaae2",
+    "url": "/static/media/heart.8b0126c6.svg"
+  },
+  {
+    "revision": "f4e74d87244be31f66d929b4196f8c75",
+    "url": "/static/media/star.f4e74d87.svg"
+  }
+]);
